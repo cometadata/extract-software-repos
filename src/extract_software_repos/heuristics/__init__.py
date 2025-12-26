319d1781131e6aa40831cafc6af4d4f7416340a7
Adam <adambuttrick@gmail.com>
2025-12-26 13:10:42 -0800

    chore: update heuristics package exports

--- src/extract_software_repos/heuristics/__init__.py
+++ src/extract_software_repos/heuristics/__init__.py
@@ -1,11 +1,15 @@
 """Heuristics for repo-paper matching."""
 
-# Import modules as they are implemented
-# Delay imports for name_similarity and author_matching until they exist
 from .arxiv_detection import detect_arxiv_id, normalize_arxiv_id, ArxivDetectionResult
+from .name_similarity import compute_name_similarity, NameSimilarityResult
+from .author_matching import match_authors_to_contributors, AuthorMatchResult
 
 __all__ = [
     "detect_arxiv_id",
     "normalize_arxiv_id",
     "ArxivDetectionResult",
+    "compute_name_similarity",
+    "NameSimilarityResult",
+    "match_authors_to_contributors",
+    "AuthorMatchResult",
 ]
